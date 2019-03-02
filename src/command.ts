226e0b672baf89eba9144b06bd97c784867af7b2
Bas Heerschop <heerschop@gmail.com>
2019-03-02 14:27:03 +0100

    0.0.0 alpha.3 (#1)
    Added development documentation.

---
+++ src/command.ts
@@ -0,0 +1,148 @@
+import { resolve } from 'path';
+import { existsSync } from 'fs';
+import { ICommand } from './config-loader';
+import { ExecOptions, SpawnOptions } from 'child_process';
+import { Process } from './spawn-process';
+import * as stringArgv from 'string-argv';
+import { Logger } from './logger';
+
+export class Command {
+  private static loadScriptLabels(): string[] {
+    const absolutePath = resolve('package.json');
+
+    if (existsSync(absolutePath)) {
+      const result = require(absolutePath);
+
+      if (result.scripts) return Object.keys(result.scripts);
+    }
+
+    return [];
+  }
+
+  private static expandArguments(text: string, args: string[]): string {
+    for (let index = 0; index < args.length; index++) {
+      const regexp = new RegExp('\\$' + index, 'g');
+
+      text = text.replace(regexp, args[index]);
+
+      if (!text.includes('$')) break;
+    }
+
+    return text;
+  }
+
+  private static expandEnvironment(text: string, environment: { [name: string]: string }): string {
+    for (const [key, value] of Object.entries(environment)) {
+      const regexp = new RegExp('\\$' + key + '([^\\w]|$)', 'g');
+
+      text = text.replace(regexp, value + '$1');
+
+      if (!text.includes('$')) break;
+    }
+
+    return text.replace(/\$\w+/g, '');
+  }
+
+  private static async spawnCommands(commands: string[], shell: string, args: string[], options: SpawnOptions, concurrent: boolean): Promise<Process[]> {
+    const processes: Process[] = [];
+    const message = concurrent ? 'Spawn concurrent: ' : 'Spawn sequential: ';
+
+    for (const command of commands) {
+      const spawnArgs = [...args, ...stringArgv(command)];
+      let scriptShell = shell;
+
+      if (!scriptShell) {
+        scriptShell = spawnArgs[0];
+        spawnArgs.shift();
+      }
+
+      Logger.log(message, scriptShell, spawnArgs);
+
+      const process = Process.spawn(scriptShell, spawnArgs, options);
+      // const process = Process.exec(scriptShell, spawnArgs, options as ExecOptions);
+
+      Logger.debug(`Process ${process.pid} started.`);
+
+      if (!concurrent) await process.wait();
+
+      processes.push(process);
+    }
+
+    return processes;
+  }
+
+  private readonly packageScripts: string[];
+  private readonly nestedShell: string;
+  private readonly args: string[];
+  private readonly environment: { [name: string]: string };
+
+  public constructor(nestedShell: string, args: string[], environment: { [name: string]: string }) {
+    this.packageScripts = Command.loadScriptLabels();
+    this.nestedShell = nestedShell;
+    this.args = args;
+    this.environment = environment;
+  }
+
+  public async execute(scriptShell: string, command: string | string[] | ICommand): Promise<number> {
+    const options: SpawnOptions = {
+      stdio: 'inherit',
+      env: this.environment,
+    };
+
+    const commands = this.prepareCommands(command);
+
+    if (commands.concurrent.length === 0 && commands.sequential.length === 0) throw new Error('missing script');
+
+    const processes: Process[] = [];
+    const args = stringArgv(scriptShell);
+
+    scriptShell = args[0];
+
+    args.shift();
+
+    processes.push(...await Command.spawnCommands(commands.concurrent, scriptShell, args, options, true));
+    processes.push(...await Command.spawnCommands(commands.sequential, scriptShell, args, options, false));
+
+    let exitCode = 0;
+
+    for (const process of processes) {
+      exitCode += await process.wait();
+    }
+
+    return exitCode;
+  }
+
+  private prepareCommands(command: string | string[] | ICommand): { concurrent: string[], sequential: string[] } {
+    const concurrent: string[] = [];
+    const sequential: string[] = [];
+
+    if (command instanceof Array) sequential.push(...command);
+    if (typeof command === 'string') sequential.push(command);
+    if (command instanceof String) sequential.push(command.toString());
+
+    if ((command as ICommand).concurrent) concurrent.push(...(command as ICommand).concurrent);
+    if ((command as ICommand).sequential) sequential.push(...(command as ICommand).sequential);
+
+    return {
+      concurrent: this.resolveReferences(concurrent),
+      sequential: this.resolveReferences(sequential),
+    };
+  }
+
+  private resolveReferences(commands: string[]): string[] {
+    const result = [...commands];
+
+    for (let index = 0; index < commands.length; index++) {
+      let command = Command.expandArguments(commands[index], this.args);
+
+      command = Command.expandEnvironment(command, this.environment);
+
+      // if (this.packageScripts.includes(command)) command = this.nestedShell + ' ' + command;
+      if (command.match(/^((\w+\:\w+)+$)/) != null) command = this.nestedShell + ' ' + command;
+
+      result[index] = command;
+    }
+
+    return result;
+  }
+}
---
+++ src/launch.ts
@@ -0,0 +1,65 @@
+#!./node_modules/.bin/ts-node --skip-project
+
+import { Config } from './config-loader';
+import { Logger } from './logger';
+import { Command } from './command';
+import { launchMenu } from './launch-menu';
+import * as fs from 'fs';
+
+async function main(): Promise<void> {
+  let exitCode = 1;
+
+  try {
+    const config = Config.load();
+
+    Logger.level = config.configurations.logLevel;
+
+    Logger.debug('Config: ', config);
+
+    const lifecycleEvent = process.env.npm_lifecycle_event;
+    const commandArgs = process.env.npm_config_argv ? JSON.parse(process.env.npm_config_argv).remain : '';
+    const scriptArgs = process.argv.slice(2, process.argv.length - commandArgs.length);
+    const scriptPattern = lifecycleEvent === 'start' ? commandArgs[0] : lifecycleEvent;
+
+    Logger.info('Lifecycle event:', lifecycleEvent);
+    Logger.info('Command arguments:', commandArgs);
+    Logger.info('Script arguments:', scriptArgs);
+
+    if (`${scriptArgs}` === 'init') {
+      const targetFile = 'script-launcher.json';
+
+      if (fs.existsSync(targetFile)) {
+        Logger.error('The file "' + targetFile + '" already exists.');
+        return;
+      }
+
+      fs.writeFileSync(targetFile, JSON.stringify(Config.init, null, 2));
+
+      return;
+    }
+
+    if (scriptPattern === undefined) {
+      exitCode = await launchMenu();
+      return;
+    }
+
+    const script = config.findScript(scriptPattern);
+    const scriptShell = config.configurations.script.scriptShell;
+    const nestedShell = config.configurations.script.nestedShell;
+    const environment = { ...process.env, ...script.parameters };
+    const command = new Command(nestedShell, commandArgs, environment);
+
+    Logger.info('Selected script:', script.name);
+    Logger.info('Parameters:', script.parameters);
+
+    exitCode = await command.execute(scriptShell, script.command);
+
+    Logger.info('ExitCode:', exitCode);
+  } catch (error) {
+    Logger.error(`${error}`);
+  } finally {
+    process.exit(exitCode);
+  }
+}
+
+main();
