0ece2d57fd61962323d6c3c2dbff0c5aef24036b
Bas Heerschop <heerschop@gmail.com>
2019-04-13 10:47:54 +0200

    wip

--- src/spawn-process.ts
+++ src/spawn-process.ts
@@ -15,34 +15,21 @@ export class Process {
 
     if (options.cwd) Logger.log('Process dir     : ' + Colors.Green + '"' + options.cwd + '"' + Colors.Normal);
 
-    if (Logger.level > 1) {
-      Logger.log('Process pid     : ' + Colors.Yellow + childProcess.pid + Colors.Normal);
-      Logger.log(''.padEnd(process.stdout.columns, '-'));
-
-      Process.showOutputData(childProcess);
-    }
-
     return new Process(childProcess, startTime);
   }
 
-  private static showOutputData(childProcess: ChildProcess): void {
-    childProcess.stdout.on('data', (data) => {
-      const content = (data.toString() as string).trim();
-      if (content) Logger.log(Colors.Dim + content + Colors.Normal);
-    });
-
-    childProcess.stderr.on('data', (data) => {
-      const content = (data.toString() as string).trim();
-      if (content) Logger.log(Colors.Red + content + Colors.Normal);
-    });
-  }
-
   public readonly pid: number;
-
+  private outputCount = 0;
   private readonly exitPromise: Promise<number>;
 
   private constructor(childProcess: ChildProcess, startTime: number) {
     this.pid = childProcess.pid;
+
+    if (Logger.level > 1) {
+      Logger.log('Process pid     : ' + Colors.Yellow + childProcess.pid + Colors.Normal);
+
+      this.showOutputData(childProcess);
+    }
 
     this.exitPromise = new Promise<number>((resolve, reject) => {
       try {
@@ -50,7 +37,8 @@ export class Process {
         childProcess.on('exit', (code, signal) => {
           const timeSpan = Date.now() - startTime;
 
-          Logger.log(''.padEnd(process.stdout.columns, '-'));
+          if (this.outputCount !== 0) Logger.log(''.padEnd(process.stdout.columns, '-'));
+
           Logger.log('Process exited  : pid=' + childProcess.pid + '  code=' + code + '  signal=' + signal, '  elapsed=' + timeSpan + ' ms');
           Logger.log();
           Logger.log();
@@ -61,14 +49,14 @@ export class Process {
         childProcess.on('error', (error) => {
           const timeSpan = Date.now() - startTime;
 
-          Logger.log(''.padEnd(process.stdout.columns, '-'));
+          if (this.outputCount !== 0) Logger.log(''.padEnd(process.stdout.columns, '-'));
           Logger.log('Process error   : pid=' + childProcess.pid + `  code=${error}`, '  elapsed=' + timeSpan + ' ms');
           Logger.log();
           Logger.log();
           reject(error);
         });
       } catch (error) {
-        Logger.log(''.padEnd(process.stdout.columns, '-'));
+        if (this.outputCount !== 0) Logger.log(''.padEnd(process.stdout.columns, '-'));
         Logger.error('Process failed  : pid=' + childProcess.pid + `  failed to attach event emitters, ${error}.`);
         Logger.log();
         Logger.log();
@@ -79,5 +67,29 @@ export class Process {
 
   public wait(): Promise<number> {
     return this.exitPromise;
+  }
+
+  private showOutputData(childProcess: ChildProcess): void {
+    childProcess.stdout.on('data', (data) => {
+      const content = (data.toString() as string).trim();
+      if (content) {
+        if (this.outputCount === 0) Logger.log(''.padEnd(process.stdout.columns, '-'));
+
+        Logger.log(Colors.Dim + content + Colors.Normal);
+
+        this.outputCount++;
+      }
+    });
+
+    childProcess.stderr.on('data', (data) => {
+      const content = (data.toString() as string).trim();
+      if (content) {
+        if (this.outputCount === 0) Logger.log(''.padEnd(process.stdout.columns, '-'));
+
+        Logger.log(Colors.Red + content + Colors.Normal);
+
+        this.outputCount++;
+      }
+    });
   }
 }
