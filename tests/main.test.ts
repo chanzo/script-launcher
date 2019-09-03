68616f2951094264108b64dd2aeb5d515d71fc26
Bas Heerschop <heerschop@gmail.com>
2019-09-03 07:18:34 +0200

    wip

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,10 +1,10 @@
-function isPalindrome(s) {
+function isPalindrome(s: string) {
   const count = s.length - 1
   if (count < 2) {
     return true
   }
 
-  for (i = 0; i < (count + 1) / 2; ++i) {
+  for (let i = 0; i < (count + 1) / 2; ++i) {
     if (s[i] !== s[count - i])
       return false
   }
