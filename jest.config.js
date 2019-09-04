79e8bca7beae8b35592001cbc47f1e81c86e188a
Bas Heerschop <heerschop@gmail.com>
2019-09-04 07:43:48 +0200

    dir rename

--- jest.config.js
+++ jest.config.js
@@ -116,7 +116,7 @@ module.exports = {
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
-    'test-methods'
+    'tests'
   ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
