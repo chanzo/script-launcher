c6286fcded76b022f9359183afdb29a0c68237df
Bas Heerschop <heerschop@gmail.com>
2020-01-28 08:32:26 +0100

    completion test stuff

--- src/launch-completion.sh
+++ src/launch-completion.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
+date -Iseconds > coml.log
 
-echo "COMP_CWORD: $COMP_CWORD"  > coml.log
+echo "COMP_CWORD: $COMP_CWORD" >> coml.log
 echo "COMP_LINE: $COMP_LINE"   >> coml.log
 echo "COMP_POINT: $COMP_POINT" >> coml.log
+echo "$@"                      >> coml.log
+
+BACKUP_COMP_CWORD=COMP_CWORD
+BACKUP_COMP_LINE=COMP_LINE
+BACKUP_COMP_POINT=COMP_POINT
+
+unset COMP_CWORD
+unset COMP_LINE
+unset COMP_POINT
 
 eval "$(npm completion)"
-# _npm_completion
+_npm_completion "$@"
+
+export COMP_CWORD=$BACKUP_COMP_CWORD
+export COMP_LINE=$BACKUP_COMP_LINE
+export COMP_POINT=$BACKUP_COMP_POINT
+
+_npm_completion
